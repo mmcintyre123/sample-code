ed28b81ac146470588a836d4dd4033a064aaada5
Matthew McIntyre <matthewtfm@gmail.com>
2016-11-29 17:49:25 -0500

    adding and testing new elements

--- sample-code/examples/node/helpers/elements.js
+++ sample-code/examples/node/helpers/elements.js
@@ -4,8 +4,15 @@
 	// to access just the last part of the ID
 	// for the purpose of .waitForElementById: split(/\//).splice(1,1)
 
+	
+	//test
+	exports.actionBar = {
+		'hamburgerMenuByXPath' :  '//android.widget.TextView[@index=1]',
+		'search'			   :  'com.i360.i360Walk:id/search',
+		'refresh'			   :  'com.i360.i360Walk:id/action_refresh',
+		'add'  				   :  'com.i360.i360Walk:id/action_add' // this usually adds a contact or volunteer, depending on the context.
+	}
 
-// All Elements are defined by their ID.  Use "elementById" in script when calling these definitions.
 	exports.loginScreen = {
 		'userName'		 : 'com.i360.i360Walk:id/etLoginUsername',
 		'password'		 : 'com.i360.i360Walk:id/etPassword',
@@ -22,17 +29,28 @@
 		'signOut'        : 'com.i360.i360Walk:id/btnSignOut'
 	}
 
+	//test - especially books off page
 	exports.walkbooks = {
-		'search'     :  'com.i360.i360Walk:id/search',
-		'survey1'    :  '',
-		'survey2'    :  '',
-		'survey3'    :  '',
-		'survey4'    :  '',
-		'survey5'    :  '',
-		'survey6'    :  '',
-		'survey7'    :  '',
-		'survey8'    :  '',
-		'survey9'    :  '',
-		'survey10'   :  '',
-		'logoutMenu' :  ''
+		'survey1ByXpath'    :  '//android.widget.ListView/android.widget.LinearLayout[@index="0"]',
+		'survey2ByXpath'    :  '//android.widget.ListView/android.widget.LinearLayout[@index="1"]',
+		'survey3ByXpath'    :  '//android.widget.ListView/android.widget.LinearLayout[@index="2"]',
+		'survey4ByXpath'    :  '//android.widget.ListView/android.widget.LinearLayout[@index="3"]',
+		'survey5ByXpath'    :  '//android.widget.ListView/android.widget.LinearLayout[@index="4"]',
+		'survey6ByXpath'    :  '//android.widget.ListView/android.widget.LinearLayout[@index="5"]',
+		'survey7ByXpath'    :  '//android.widget.ListView/android.widget.LinearLayout[@index="6"]',
+		'survey8ByXpath'    :  '//android.widget.ListView/android.widget.LinearLayout[@index="7"]',
+		'survey9ByXpath'    :  '//android.widget.ListView/android.widget.LinearLayout[@index="8"]',
+		'survey10ByXpath'   :  '//android.widget.ListView/android.widget.LinearLayout[@index="9"]'
+	}
+
+	//test
+	exports.survey = {
+		'previewQuestions' 	 :  'com.i360.i360Walk:id/tvSurveyPreview',		
+		'start' 			 :  'com.i360.i360Walk:id/btnStartSurvey',		
+		'gpsLocateMeByXPath' :  '//android.widget.TabHost/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.FrameLayout[@index="2"]/android.widget.ImageView',
+		'map' 				 :  '//android.widget.TextView[@text="Map"]',
+		'list' 				 :  '//android.widget.TextView[@text="List"]',
+		'walkbook1ByXpath' 	 :  '//android.widget.TabHost/android.view.View[@index="0"]',
+		'walkbook2ByXpath' 	 :  '//android.widget.TabHost/android.view.View[@index="1"]',
+		'walkbook3ByXpath' 	 :  '//android.widget.TabHost/android.view.View[@index="2"]'
 	}
--- sample-code/examples/node/walk_sandbox.js
+++ sample-code/examples/node/walk_sandbox.js
@@ -32,6 +32,52 @@ describe("android local server", function () {
 
 // Tests
 
+  it('should successfully select the first 10 surveys in the list ', function (done) {
+    var walkbookID = elements.homeScreen.walkbooks.split(/\//).splice(1,1).toString()
+    var a = config.driver
+
+    return a
+
+      .elementById(elements.loginScreen.rememberMe)
+        .click()
+      .elementById(elements.loginScreen.userName)
+        .sendKeys('test_1654wseward')
+      .elementById(elements.loginScreen.password)
+        .sendKeys('asdf')
+      .elementById(elements.loginScreen.logIn)
+        .click().sleep(1000)
+      .waitForElementById(walkbookID, 5000)
+
+      // Open Walkbooks - Survey List
+      .elementById(elements.homeScreen.walkbooks)
+        .click().sleep(3000)
+
+      // Open each survey in the list.
+      .elementByXPath(elements.walkbooks.survey1ByXpath).click().sleep(1000)
+        .back().sleep(1000)
+      .elementByXPath(elements.walkbooks.survey2ByXpath).click().sleep(1000)
+        .back().sleep(1000)
+      .elementByXPath(elements.walkbooks.survey3ByXpath).click().sleep(1000)
+        .back().sleep(1000)
+      .elementByXPath(elements.walkbooks.survey4ByXpath).click().sleep(1000)
+        .back().sleep(1000)
+      .elementByXPath(elements.walkbooks.survey5ByXpath).click().sleep(1000)
+        .back().sleep(1000)
+      .elementByXPath(elements.walkbooks.survey6ByXpath).click().sleep(1000)
+        .back().sleep(1000) 
+
+      // LEFT OFF HERE 
+      // must scroll screen before this
+      .elementByXPath(elements.walkbooks.survey7ByXpath).click().sleep(1000)
+        .back().sleep(1000)
+
+      done();
+
+  });
+
+
+
+
   it("is a sandbox. Commence testing!", function () {
     var walkbookID = elements.homeScreen.walkbooks.split(/\//).splice(1,1).toString()
     var a = config.driver
@@ -45,158 +91,40 @@ describe("android local server", function () {
       .elementById(elements.loginScreen.password)
         .sendKeys('asdf')
       .elementById(elements.loginScreen.logIn)
-        .click()
+        .click().sleep(1000)
       // .waitForElementById('btnWalkbooks', 3000) // works
       .waitForElementById(walkbookID, 5000)
 
       // Open Walkbooks - Survey List
       .elementById(elements.homeScreen.walkbooks)
-        .click().sleep(1000)
-      // .elementById('com.i360.i360Walk:id/lvQuestions')
+        .click().sleep(3000)
 
-      // .then(function () {eval(pry.it); return this;})
-      //     .elementByXPath('//android.widget.LinearLayout[@index="0"]').click()
-
-      // .then(function () {eval(pry.it); return this;})
-      //     .elementsByXPath('//android.widget.LinearLayout[contains(@index, "0")]').click()
-
-      // .then(function () {eval(pry.it); return this;})
-      //     .elementByXPath('//android.widget.FrameLayout[0]/android.view.ViewGroup[0]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[0]/android.widget.FrameLayout[0]/android.widget.RelativeLayout[0]/android.widget.ListView[0]/android.widget.LinearLayout[@index = 0]')
-      //       .click()
-
-      .then(function () {debugger; return this;})
-      // .then(function () {eval(pry.it); return this;})
-      .then(function (result) {
-          try {
-            return config.driver
-            .elementByXPath('//android.widget.FrameLayout[0]/android.view.ViewGroup[0]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[0]/android.widget.FrameLayout[0]/android.widget.RelativeLayout[0]/android.widget.ListView[0]/android.widget.LinearLayout[@index = \'0\']')
-          } catch (error) {
-              done(new Error('Test failed.  Moving on.'));
-          }
-        })
-
-      .then(function () {debugger; return this;})
-      // .then(function () {eval(pry.it); return this;})
-      return config.driver
-        .then(function () {
-          try {
-          return config.driver
-          .elementById(elements.homeScreen.walkbooks)
-            .click().sleep(1000)
-          .elementByXPath("//android.widget.LinearLayout[@index='0']/android.widget.TextView[@index='0']")
-          } catch (error) {}
-        })
-
-      .then(function () {debugger; return this;})
-      // .then(function () {eval(pry.it); return this;})
-          .elementById(elements.homeScreen.walkbooks)
-            .click().sleep(1000)
-          var surveys = config.driver.elementsById('com.i360.i360Walk:id/lvQuestions')
-
-
-
-
-
-
-// By.xpath("//*[@class='android.widget.ImageView'][@NAF='true']");
-
-
-.elementById('android:id/content')
-.elementById('com.i360.i360Walk:id/Body')
-.elementById('com.i360.i360Walk:id/RelativeBody')
-.elementById('com.i360.i360Walk:id/lvQuestions')
-
-
-
-
-//******** LEFT OFF HERE *************//
-
-      // Select the first survey
-
-      .elementsById('com.i360.i360Walk:id/lvQuestions')
-      .linearLayoutElement('6').click()
-
-      
-
-      // .frameLayoutChildren('com.i360.i360Walk:id/Body')
-      // .then(function(els){
-      //   return els[1].click();
-      // })
-
-      // .elementById('com.i360.i360Walk:id/lvQuestions')
-      // .elementByXPath('//android.widget.LinearLayout[@index(0)]')
-      //   .click()
-    });
 /*
-
-  it('should try clicking the first survey', function () {
-    var walkbookID = elements.homeScreen.walkbooks.split(/\//).splice(1,1).toString()
-    return config.driver
-      .waitForElementById(walkbookID, 3000)
-      // .then(function () {console.log('before here we go'); return this;})
-      .linearLayoutChildren('1')
-      // .then(function () {console.log('after here we go'); return this;})
-      .then(function(els) {
-          return els[1].click();
-      });
-    });
-
+      // Select the first survey in the list
+      .elementByXPath('//android.widget.ListView/android.widget.LinearLayout[@index="0"]')
+        .click()
 */
 
-//    it('should try some things on the Walkbooks page', function () {
-//        // should press the logout menu button
-//        return config.driver
-//  
-//      /*  
-//        .elementById('android:id/action_bar')
-//        .then(function (opts) {
-//          var action = new wd.TouchAction();
-//          action
-//            .press({x: 1328, y: 192})
-//            .release();
-//          return this.performTouchAction(action);
-//        });
-//  
-//  
-//        .then(function () {
-//        var a1 = new wd.TouchAction()
-//        a1.press({x: 1328, y: 192}).perform();
-//      */
-//  
-//        // Logout from top right menu
-//        .elementById('android:id/action_bar')
-//        .elementByXPath('//android.widget.TextView[@index=1]')
-//          .click().sleep(100)
-//        .elementByXPath("//android.widget.TextView[@text='Log Out']")
-//          .click().sleep(100)
-//        .elementById('android:id/button1') // OK - confirm logout
-//          .click()
-//    });
-//  
-//    it('should click on the first survey', function () {
-//      var walkbookID = elements.homeScreen.walkbooks.split(/\//).splice(1,1).toString()
-//      return config.driver
-//      .sleep(2000)
-//      //login
-//      .elementById(elements.loginScreen.rememberMe)
-//        .click()
-//      .elementById(elements.loginScreen.userName)
-//        .sendKeys('test_1654wseward')
-//      .elementById(elements.loginScreen.password)
-//        .sendKeys('asdf')
-//      .elementById(elements.loginScreen.logIn)
-//        .click()
-//      // .waitForElementById('btnWalkbooks', 3000) // works
-//      .waitForElementById(walkbookID, 3000)
-//  
-//      // Open Walkbooks - Survey List
-//      .elementById(elements.homeScreen.walkbooks)
-//        .click().sleep(1000)
-//  
-//      // Select the first survey
-//      .elementByXPath("//android.widget.LinearLayout[@index(0)]")
-//      .elementById('com.i360.i360Walk:id/rlRowItem')
-//        .click()
-//    });
+      // Logout from top right menu
+        .elementByXPath('//android.widget.TextView[@index=1]')
+          .click().sleep(100) // Hamburger menu
+        // .elementById('android:id/title')
+        .elementByName('Log Out')
+          .click().sleep(100) // Log Out
+        .elementById('android:id/button1')
+          .click() // OK - confirm logout
 
+
+
+/*
+      // .then(function () {eval(pry.it); return this;})
+      .then(function () {debugger; return this;})
+          .elementByClassName('android.widget.ListView')
+          .elementByXPath('//android.widget.LinearLayout[@index="2"]').click() // should be "and another one"
+
+      .then(function () {debugger; return this;})
+          .elementByXPath('//android.widget.LinearLayout[@index="2"]').click() // is this "and another one?"
+*/
+
+    });
 });
