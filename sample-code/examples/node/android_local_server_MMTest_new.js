ecca30e91a8d5f7fe8180a5db3f811cd84fb0d06
Matthew McIntyre <matthewtfm@gmail.com>
2016-12-06 11:59:17 -0500

    updates

--- sample-code/examples/node/android_local_server_MMTest_new.js
+++ sample-code/examples/node/android_local_server_MMTest_new.js
@@ -1,6 +1,14 @@
 "use strict";
 
+require('colors');
 require("./helpers/setup");
+
+var actions  =  require('./helpers/actions');
+var _p       =  require('./helpers/promise-utils');  // needed?
+var _ = require('underscore');
+var wd       =  require("wd"); // - move to config?
+var Q                    =  require('q');
+
 
 var elements  = require('./helpers/elements')
 var simulator = false
@@ -11,15 +19,19 @@ var config  = require('./helpers/config')
 chai.config.includeStack = true;
 var commons = require('./helpers/commons').Commons;
 var commonMethods = new commons();
+var driver = config.driver;
+var pry  =  require('pryjs');
 // var actions = require('./helpers/actions')
-// wd.addPromiseChainMethod('swipe', actions.swipe);
+
+wd.addPromiseChainMethod('swipe', actions.swipe);
 
 
 describe("android local server", function () {
 
 // Set up stuff
 
-  this.timeout(30000);
+  // this.timeout(30000);
+  this.timeout(33333333);
   commonMethods.beforeAll();
   commonMethods.afterAll();
   commonMethods.afterEach();
@@ -27,24 +39,25 @@ describe("android local server", function () {
 // Tests 
 
   it("should open the app", function () {
-    return config.driver
+    var walkbookID = elements.homeScreen.walkbooks.split(/\//).splice(1,1).toString();
+    return driver
+
       .elementById(elements.loginScreen.rememberMe)
-        .click().sleep(500)
+        .click()
       .elementById(elements.loginScreen.userName)
         .sendKeys('test_1654wseward')
       .elementById(elements.loginScreen.password)
         .sendKeys('asdf')
       .elementById(elements.loginScreen.logIn)
-        .click()
-      var walkbookID = elements.homeScreen.walkbooks.split(/\//).splice(1,1)
-      .waitForElementById(walkbookID, 3000);
+        .click().sleep(1000)
+      .waitForElementById(walkbookID, 5000)
   });
 
   it("should open Walkbooks and Preview the Survey", function() {
-    var previewSource;
-    return config.driver
-      .elementByName('Walkbooks')
-        .click().sleep(1000)
+    var previewSource
+    return driver
+      .elementById(elements.homeScreen.walkbooks)
+        .click().sleep(3000)
       .waitForElementByName('and another one', 2000)
       .elementByName('and another one')
         .click()
@@ -63,7 +76,7 @@ describe("android local server", function () {
     });
 
   it('should select a walkbook, household, target, and take a survey', function () {
-    return config.driver
+    return driver
       // Select a walkbook, list view, make assertions
       .back()
       .elementByName('Start')
@@ -80,8 +93,8 @@ describe("android local server", function () {
 
       // Select a house & assertions
 
-      .waitForElementByName('Keene')
-      .elementByName('Keene')
+      .waitForElementByName('Parrott')
+      .elementByName('Parrott')
         .click()
 
         //ASSERTIONS
@@ -97,19 +110,20 @@ describe("android local server", function () {
 
       // take survey
 
-//      .waitForElementById('btnTakeSurvey', 2000 )
-//      .elementByName('Take Survey')
-//        .click()
-//
-//        //ASSERTIONS
-//
-//      .waitForElementByName('Answer 1', 2000)
-//      .elementByName('Answer 1')
-//        .click()
-//      .elementByName('Next')
-//        .click()
-//      .waitForElementById('Finished with Household', 2000)
-//      .elementByName('Finished with Household')
-//        .click()
+      .waitForElementById('btnTakeSurvey', 2000 )
+      .elementByName('Take Survey')
+        .click()
+
+        //ASSERTIONS
+
+      .waitForElementByName('Answer 1', 2000)
+      .elementByName('Answer 1')
+        .click()
+      .elementByName('Next')
+        .click()
+      .then(function () {eval(pry.it); return this;})
+      .waitForElementById('Finished with Household', 2000)
+      .elementByName('Finished with Household')
+        .click()
   });
 });
--- sample-code/examples/node/walk_sandbox.js
+++ sample-code/examples/node/walk_sandbox.js
@@ -1,13 +1,14 @@
 "use strict";
 
+require('colors');
 require("./helpers/setup");
 
-// var actions  =  require('./helpers/actions');
-// var _p       =  require('./helpers/promise-utils');  // needed?
-// var Q        =  require('q'); // needed?
-// var wd       =  require("wd"); // - move to config?
+var actions  =  require('./helpers/actions');
+var _p       =  require('./helpers/promise-utils');  // needed?
+var _ = require('underscore');
+var wd       =  require("wd"); // - move to config?
+var Q                    =  require('q');
 // var args     =  process.args.slice(2);
-// wd.addPromiseChainMethod('swipe', actions.swipe);
 
 var elements             =  require('./helpers/elements')
 var simulator            =  false
@@ -18,12 +19,15 @@ var config               =  require('./helpers/config')
 chai.config.includeStack =  true;
 var commons              =  require('./helpers/commons').Commons;  // new
 var commonMethods        =  new commons();  // new
-var pry                  =  require('pryjs')
+var driver               =  config.driver;
+var pry                  =  require('pryjs');
+
+
+wd.addPromiseChainMethod('swipe', actions.swipe);
 
 describe("android local server", function () {
 
 // Set up stuff
-
   // this.timeout(30000); // use this for testing
   this.timeout(33333333); // use this for debugging with pry
   commonMethods.beforeAll();
@@ -32,11 +36,10 @@ describe("android local server", function () {
 
 // Tests
 
-  it('should successfully select the first 10 surveys in the list ', function (done) {
+  it('should successfully select the first 10 surveys in the list '.green, function () {
     var walkbookID = elements.homeScreen.walkbooks.split(/\//).splice(1,1).toString()
-    var a = config.driver
 
-    return a
+    return driver
 
       .elementById(elements.loginScreen.rememberMe)
         .click()
@@ -53,36 +56,74 @@ describe("android local server", function () {
         .click().sleep(3000)
 
       // Open each survey in the list.
-      .elementByXPath(elements.walkbooks.survey1ByXpath).click().sleep(1000)
-        .back().sleep(1000)
-      .elementByXPath(elements.walkbooks.survey2ByXpath).click().sleep(1000)
-        .back().sleep(1000)
-      .elementByXPath(elements.walkbooks.survey3ByXpath).click().sleep(1000)
-        .back().sleep(1000)
-      .elementByXPath(elements.walkbooks.survey4ByXpath).click().sleep(1000)
-        .back().sleep(1000)
-      .elementByXPath(elements.walkbooks.survey5ByXpath).click().sleep(1000)
-        .back().sleep(1000)
-      .elementByXPath(elements.walkbooks.survey6ByXpath).click().sleep(1000)
-        .back().sleep(1000) 
+//      .elementByXPath(elements.walkbooks.survey1ByXpath).click().sleep(1000)
+//        .back().sleep(1000)
+//      .elementByXPath(elements.walkbooks.survey2ByXpath).click().sleep(1000)
+//        .back().sleep(1000)
+//      .elementByXPath(elements.walkbooks.survey3ByXpath).click().sleep(1000)
+//        .back().sleep(1000)
+//      .elementByXPath(elements.walkbooks.survey4ByXpath).click().sleep(1000)
+//        .back().sleep(1000)
+//      .elementByXPath(elements.walkbooks.survey5ByXpath).click().sleep(1000)
+//        .back().sleep(1000)
+//      .elementByXPath(elements.walkbooks.survey6ByXpath).click().sleep(1000)
+//        .back().sleep(1000) 
 
-      // LEFT OFF HERE 
-      // must scroll screen before this
-      .elementByXPath(elements.walkbooks.survey7ByXpath).click().sleep(1000)
-        .back().sleep(1000)
+  });
 
-      done();
+
+  it('Should scroll and click the last survey - First Attempt', function () {
+    return driver
+    
+    // scroll the page
+    
+    .elementByXPath(elements.walkbooks.survey4ByXpath)
+    .getLocation()
+    .then(function (loc) {
+      return driver.swipe({
+              startX: loc.x, startY: loc.y,
+              endX: loc.x, endY: loc.y - 800,
+              duration: 800
+            });
+          })
+    .sleep(1000)
+    // .elementByXPath(elements.walkbooks.survey6ByXpath)
+    // .isDisplayed().should.eventually.be.true
+
+    .then(function myFunction(){
+      var myels = driver.elementsByXPath('//android.widget.ListView/android.widget.LinearLayout')
+      console.log(Object.keys(myels))
+      driver.sleep(2000)
+      console.log(myels)
+      return myels
+    })
+
+    .then(function () {debugger; return this;})
+    // .then(function () {eval(pry.it); return this;})
+
+    .elementByXPath(elements.walkbooks.survey6ByXpath)
+      .click().sleep(1000)
+      .back().sleep(1000) // should open "MM Test Dynamic Survey - last survey in list"
+  });
+
+
+  it('should count the number of surveys in view', function () {
+    return driver
+
+
 
   });
 
 
 
 
+
+
+/*
   it("is a sandbox. Commence testing!", function () {
     var walkbookID = elements.homeScreen.walkbooks.split(/\//).splice(1,1).toString()
-    var a = config.driver
 
-    return config.driver
+    return driver
       //login
       .elementById(elements.loginScreen.rememberMe)
         .click()
@@ -99,11 +140,11 @@ describe("android local server", function () {
       .elementById(elements.homeScreen.walkbooks)
         .click().sleep(3000)
 
-/*
-      // Select the first survey in the list
-      .elementByXPath('//android.widget.ListView/android.widget.LinearLayout[@index="0"]')
-        .click()
-*/
+
+//      // Select the first survey in the list
+//      .elementByXPath('//android.widget.ListView/android.widget.LinearLayout[@index="0"]')
+//        .click()
+//
 
       // Logout from top right menu
         .elementByXPath('//android.widget.TextView[@index=1]')
@@ -114,17 +155,14 @@ describe("android local server", function () {
         .elementById('android:id/button1')
           .click() // OK - confirm logout
 
-
-
-/*
-      // .then(function () {eval(pry.it); return this;})
-      .then(function () {debugger; return this;})
-          .elementByClassName('android.widget.ListView')
-          .elementByXPath('//android.widget.LinearLayout[@index="2"]').click() // should be "and another one"
-
-      .then(function () {debugger; return this;})
-          .elementByXPath('//android.widget.LinearLayout[@index="2"]').click() // is this "and another one?"
-*/
+//      .then(function () {eval(pry.it); return this;})
+//      .then(function () {debugger; return this;})
+//          .elementByClassName('android.widget.ListView')
+//          .elementByXPath('//android.widget.LinearLayout[@index="2"]').click() // should be "and another one"
+//
+//      .then(function () {debugger; return this;})
+//          .elementByXPath('//android.widget.LinearLayout[@index="2"]').click() // is this "and another one?"
 
     });
+  */
 });
